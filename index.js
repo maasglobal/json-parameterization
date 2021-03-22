d22c6713164633c77cdc7ab4eb687bf8e9d09957
hieuunguyeen <blackevil245@gmail.com>
2021-03-22 15:36:51 +0200

    Patch null check

--- index.js
+++ index.js
@@ -20,15 +20,13 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-var _           = require('lodash');
-var debug       = require('debug')('json-parameterization');
-
+var _ = require("lodash");
+var debug = require("debug")("json-parameterization");
 
 /** Parameterize input with params */
-var parameterize = function(input, params) {
-
+var parameterize = function (input, params) {
   // Evaluate an expression
-  var evalExpr = function(expr, fallback) {
+  var evalExpr = function (expr, fallback) {
     // Find parts of the expression
     var parts = expr.split(/\|/);
 
@@ -36,7 +34,7 @@ var parameterize = function(input, params) {
     // Check if first value is on a "string" format
     var match = /\s*"([^"]*)"\s*|\s*'([^']*)'\s*/.exec(value);
     if (match) {
-      value = match[1] || match[2] || '';
+      value = match[1] || match[2] || "";
     } else {
       // Otherwise, trim and look up in parameters
       var result = params[value.trim()];
@@ -48,8 +46,7 @@ var parameterize = function(input, params) {
         value = _.cloneDeep(result);
       } else {
         // If we didn't get a value, function or string
-        debug("Couldn't find parameter: '%s' used in '%s'",
-              value, expr);
+        debug("Couldn't find parameter: '%s' used in '%s'", value, expr);
         return fallback;
       }
     }
@@ -75,34 +72,37 @@ var parameterize = function(input, params) {
   };
 
   // Parameterize a string
-  var parameterizeString = function(str) {
+  var parameterizeString = function (str) {
     // Handle special case where the entire string is an expression
     if (/^{{[^}]+}}$/.test(str)) {
       return evalExpr(str.substr(2, str.length - 4), str);
     }
 
     // Otherwise treat {{...}} as string interpolations
-    return str.replace(/{{([^}]*)}}/g, function(originalText, expression) {
+    return str.replace(/{{([^}]*)}}/g, function (originalText, expression) {
       return evalExpr(expression, originalText);
     });
   };
 
   // Substitute objects that needs this
-  var substitute = function(obj) {
+  var substitute = function (obj) {
     // Parameterized strings
-    if (typeof(obj) == 'string') {
+    if (typeof obj == "string") {
       return parameterizeString(obj);
     }
 
     // Parameterize keys of objects
-    if (typeof(obj) == 'object') {
+    if (typeof obj == "object") {
+      if (obj === null) {
+        return obj;
+      }
       if (obj instanceof Array) {
         return undefined;
       }
       var clone = {};
-      for(var k in obj) {
+      for (var k in obj) {
         var key = parameterizeString(k);
-        if (typeof(key) !== 'string') {
+        if (typeof key !== "string") {
           key = k;
         }
         clone[key] = _.cloneDeepWith(obj[k], substitute);
@@ -117,7 +117,6 @@ var parameterize = function(input, params) {
   // Create clone while substituting objects that need it
   return _.cloneDeepWith(input, substitute);
 };
-
 
 // Export parameterize
 module.exports = parameterize;
